a43337ffefa7e1969ed28c0af17b0aa7e723befb
KirubhaChezhiyan <kirubhach@gmail.com>
2023-02-13 22:39:19 +0530

    V0

---
+++ Main1.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+
+   /* public class Main1 {
+    public static void main(String[] args) {
+        Square s1 = new Square();
+        s1.length = 10f;
+        System.out.println("Area of the Square:"+s1.area());
+        Rectangle r1 = new Rectangle();
+        r1.length = 5f;
+        r1.breadth = 3f;
+        System.out.println("Area of the Square:"+r1.area());
+        Circle c1 = new Circle(7f);
+        System.out.println("Area of the Circle:"+c1.area());
+    }
+}
+class Circle
+{
+
+
+    float radius;
+        public Circle(float radius)
+        {
+            this.radius = radius;
+        }
+
+
+    public float area()
+    {
+        return 3.141f*radius*radius;
+    }
+}
+class Square {
+    float length;
+
+    public float area() {
+        return length * length;
+    }
+}
+
+class Rectangle
+{
+    float length, breadth;
+    public float area()
+    {
+        return length*breadth;
+    }
+}
+
+    */
+
